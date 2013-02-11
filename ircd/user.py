marcuswanner/nameless-ircd

commit d4ccf999229c075d2b40ad3460e0d47f9a648e48

fixed error in NAMES

ircd/user.py
@@ -188,11 +188,12 @@ class User:
             chans = p[1].split(',')
             for chan in chans:
                 chan = util.filter_unicode(chan.strip())
-                self.join_chan(chan)
+                if len(chan) > 1:
+                    self.join_chan(chan)
         if data.startswith('names'):
             for chan in p[1].split(','):
                 if chan in self.chans:
-                    self.server[chan].send_who(self)
+                    self.server.chans[chan].send_who(self)
         if data.startswith('list'):
             self.server.send_list(self)
 

commit 2410248ba0d794572710f5fd7ba94f695eabea21

Fix total chan breakage in d4ccf9992

ircd/user.py
@@ -170,7 +170,7 @@ class User:
                     self.part_chan(chan)
         if data.startswith('privmsg'):
             c = inbuffer.split(':')
-            msg+= ':'.join(c[1:])
+            msg = ':'.join(c[1:])
             target = p[1]
             self.server.privmsg(self,target,msg)
         if data.startswith('topic'):